7c0cf529ae01ddbdeb124a85df76360e2fdc6e03
jmbury <justyna.m.bury@gmail.com>
2022-03-14 19:53:56 +0100

    open and show account

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
+
 public class Account {
 
     private int accountNo;
     private String accountName;
     private String accountSurname;
     private long balance;
+
+    Scanner scanner = new Scanner(System.in);
 
     public Account(int accountNo, String accountName, String accountSurname, long balance) {
         this.accountNo = accountNo;
@@ -23,5 +27,22 @@ public class Account {
 
     public void setAccountNo(int accountNo) {
         this.accountNo = accountNo;
+    }
+
+    public void openAccount() {
+        System.out.println("Podaj numer ID: ");
+        accountNo = scanner.nextInt();
+        System.out.println("Podaj imie: ");
+        accountName = scanner.next();
+        System.out.println("Podaj nazwisko: ");
+        accountSurname = scanner.next();
+        System.out.println("Podaj stan konta: ");
+        balance = scanner.nextInt();
+
+    }
+
+    public void showAcoount(){
+        System.out.println("ID: " + accountNo + " Imie: " + accountName + " Nazwisko: "+ accountSurname + " Stan konta: " + balance);
+
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
-        Account user1 = new Account(101, "Jan", "Kowalski", 10000);
+//        Account user1 = new Account(101, "Jan", "Kowalski", 10000);
         Account user2 = new Account();
 
-        System.out.println(user1.getAccountNo());
-        user2.setAccountNo(123);
-        System.out.println(user2.getAccountNo());
+//        System.out.println(user1.getAccountNo());
+        user2.openAccount();
+        user2.showAcoount();
 
 
 

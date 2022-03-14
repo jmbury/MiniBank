969560443a3cf615a5f96b10c1da2bd079d12f8d
jmbury <justyna.m.bury@gmail.com>
2022-03-14 20:18:41 +0100

    table and new accounts loop

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
 //        Account user1 = new Account(101, "Jan", "Kowalski", 10000);
-        Account user2 = new Account();
+//        Account user2 = new Account();
 
 //        System.out.println(user1.getAccountNo());
-        user2.openAccount();
-        user2.showAcoount();
+//        user2.openAccount();
+//        user2.showAccount();
 
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Ilu uzytkownikow chcesz dodac?");
+        int numberOfNewAccounts = scanner.nextInt();
+
+        Account tableOfNewAccounts[] = new Account[numberOfNewAccounts];
+
+        for (int i = 0; i < tableOfNewAccounts.length; i++) {
+            tableOfNewAccounts[i] = new Account();
+            tableOfNewAccounts[i].openAccount();
+        }
 
 
     }
